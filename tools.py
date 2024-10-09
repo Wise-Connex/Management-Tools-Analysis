4b49ee4c4d6f2c377e8f60d71ba6997f1f7fdabf
Dimar Anez <dimar@wiseconnex.com>
2024-10-09 11:07:35 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   tools.py

--- tools.py
+++ tools.py
@@ -3,16 +3,48 @@
 # *************************************************************************************
 
 # Estructura:
-#   "Título para el menú": ["nombre_del_archivo.csv de GT",
-#                          ["keywords","en el mismo orden", "como aparecen en el archivo", 
-#                               "y escritos de forma identica a como aparecen en el archivo"],
-#                           "nombre_del_archivo.csv de GB", "nombre_del_archivo.csv de Bein", ]
+#   "Título para el menú igual": 
+#        ["nombre_del_archivo.csv de GT",
+#        ["keywords","en el mismo orden", "como aparecen en el archivo", "y escritos de forma identica a como aparecen en el archivo"],
+#        "nombre_del_archivo.csv de GB", 
+#        "nombre_del_archivo.csv de Bein", 
+#        "nombre_del_archivo.csv de CR"]
+
 tool_file_dic = {
-    "Just in Time": ["justintime.csv", ["Just in Time"],"GB_Just_in_time.csv", "", "CR_Just_in_Time.csv"],
-    "Outsourcing": ["outsourcing.csv", ["Outsourcing"],"GB_Outsourcing.csv", "", "CR_Outsourcing.csv"],
-    "Project Management": ["projectmanagement.csv", ["Project Management"],"GB_Project_Management.csv", "", "CR_Project_Management.csv"],
-    "Strategic Planning": ["strategic_planning.csv", ["Strategic Planning"],"", "", "CR_Strategic_Planning.csv"],
-    "Outsourcing - Six Sigma": ["outsourcingsixsigma.csv", ["Outsourcing", "Six Sigma"],"", "", ""],
-    "Outsourcing - Strategic Planning": ["Outso_Strategic.csv", ["Strategic Planning","Outsourcing"],"", "", ""],
-    # add more pairs here
+    "Just in Time": 
+        ["justintime.csv", 
+        ["Just in Time"],
+        "GB_Just_in_time.csv", 
+        "", 
+        "CR_Just_in_Time.csv"],
+    "Outsourcing": 
+        ["outsourcing.csv", 
+        ["Outsourcing"],
+        "GB_Outsourcing.csv", 
+        "", 
+        "CR_Outsourcing.csv"],
+    "Project Management": 
+        ["projectmanagement.csv", 
+        ["Project Management"],"GB_Project_Management.csv", 
+        "", 
+        "CR_Project_Management.csv"],
+    "Strategic Planning": 
+        ["strategic_planning.csv", 
+        ["Strategic Planning"],
+        "GB_Strategic_Planning.csv", 
+        "", 
+        "CR_Strategic_Planning.csv"],
+    "Outsourcing - Six Sigma": 
+        ["outsourcingsixsigma.csv", 
+        ["Outsourcing", "Six Sigma"],
+        "", 
+        "", 
+        ""],
+    "Outsourcing - Strategic Planning": 
+        ["Outso_Strategic.csv", 
+        ["Strategic Planning","Outsourcing"],
+        "", 
+        "", 
+        ""],
+    # add more entries here
 }
